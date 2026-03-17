43720d0d42eb82d59488603f262d148c3161e40e
kaushik9n <157932311+kaushik9n@users.noreply.github.com>
2026-03-17 17:59:08 -0400

    Configure required providers for AzureRM
    Add required_providers block for azurerm provider

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-provider "azurearm" {
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
 
+provider "azurerm" {
+  features {}
+}
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
